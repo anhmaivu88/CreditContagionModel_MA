9716165dadf389d2381a8f1fa0cfe2a6f1333246
thongle <lpthong90@gmail.com>
2017-05-26 04:30:30 +0700

    add utils + model formulas

---
+++ model/fomulas.py
@@ -0,0 +1,28 @@
+import random
+import numpy as np
+
+def repay_amount():
+    amount = 0
+    return amount
+
+def size_score():
+    score = 0
+    return score
+
+def relation_score():
+    score = 0
+    return score
+
+# weight = [w_1, w_2, ...]
+# score  = [s_1, s_2, ...]
+def total_score(weight, score):
+    score = np.dot(weight, score)
+    return score
+
+def lending_decision():
+    p = 0
+    return p
+
+def lending_amount():
+    amount = 0
+    return amount
